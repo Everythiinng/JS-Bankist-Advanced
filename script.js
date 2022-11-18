f417ea03e908e4959a1191c4c8de4c74b104232b
Arbnor Rexhepi <r.arbnor@outlook.com>
2022-11-18 20:15:49 +0100

    Added the fade animation to the nav

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+const nav = document.querySelector('.nav');
 
 const openModal = function (e) {
   e.preventDefault(); // We use preventDefault cuz we have the href=#, we use this so the website doesnt jump to start after clicking
@@ -86,9 +90,6 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 
 //////////////////////////////////////////
 // Tabbed Component - Event delegation
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
 
 tabsContainer.addEventListener('click', function (e) {
   // Matching Strategy
@@ -114,3 +115,22 @@ tabsContainer.addEventListener('click', function (e) {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
 });
+
+///////////////////////////////////////////
+// Menu fade animation
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+nav.addEventListener('mouseover', handleHover.bind(0.5)); // mouseover = bubbles, mouseenter = doesnt bubble
+
+nav.addEventListener('mouseout', handleHover.bind(1)); // mouseout = mouse leaves the element
